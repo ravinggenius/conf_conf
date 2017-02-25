0044457e20d22192a01f4c59a046278f5bf84d5a
Thomas Ingram <code@ravinggenius.com>
2017-02-24 22:55:40 -0500

    Prefer class syntax

--- index.js
+++ index.js
@@ -9,51 +9,53 @@ const identity = function (value) {
 	return value;
 };
 
-const ConfConf = function (raw) {
-	this[_raw] = raw;
-};
-
-ConfConf.prototype.config = function (name, optionsOrFilter, filter) {
-	let options;
-
-	if (typeof optionsOrFilter === 'function') {
-		options = defaultOptions;
-		filter = optionsOrFilter;
-	} else {
-		options = optionsOrFilter || defaultOptions;
-		filter = filter || identity;
-	}
-
-	const rawName = options.from || humps.decamelize(name).toUpperCase();
-	const rawValue = this[_raw][rawName];
-
-	if ((rawValue === undefined) && (options.default === undefined)) {
-		throw new ConfConfError(`Missing value for \`${name}\``);
-	} else if (options.enum && !options.enum.includes(rawValue)) {
-		throw new ConfConfError(`Value for \`${name}\` must be one of ${options.enum.join(', ')}`);
-	} else {
-		this[name] = filter(rawValue || options.default);
-	}
-};
-
 const ConfConfError = function (message) {
 	this.message = message;
 };
 
-ConfConf.configure = function (rawOrSetup, setup) {
-	let raw;
-
-	if (typeof rawOrSetup === 'function') {
-		raw = process.env;
-		setup = rawOrSetup;
-	} else {
-		raw = rawOrSetup || process.env;
-		setup = setup || identity;
+const ConfConf = class {
+	constructor(raw) {
+		this[_raw] = raw;
 	}
 
-	const reply = new ConfConf(raw);
-	setup(reply);
-	return reply;
+	config(name, optionsOrFilter, filter) {
+		let options;
+
+		if (typeof optionsOrFilter === 'function') {
+			options = defaultOptions;
+			filter = optionsOrFilter;
+		} else {
+			options = optionsOrFilter || defaultOptions;
+			filter = filter || identity;
+		}
+
+		const rawName = options.from || humps.decamelize(name).toUpperCase();
+		const rawValue = this[_raw][rawName];
+
+		if ((rawValue === undefined) && (options.default === undefined)) {
+			throw new ConfConfError(`Missing value for \`${name}\``);
+		} else if (options.enum && !options.enum.includes(rawValue)) {
+			throw new ConfConfError(`Value for \`${name}\` must be one of ${options.enum.join(', ')}`);
+		} else {
+			this[name] = filter(rawValue || options.default);
+		}
+	}
+
+	static configure(rawOrSetup, setup) {
+		let raw;
+
+		if (typeof rawOrSetup === 'function') {
+			raw = process.env;
+			setup = rawOrSetup;
+		} else {
+			raw = rawOrSetup || process.env;
+			setup = setup || identity;
+		}
+
+		const reply = new ConfConf(raw);
+		setup(reply);
+		return reply;
+	}
 };
 
 module.exports = ConfConf;
